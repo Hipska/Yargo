b42a478df486fdcf4e11fc30815d040cf64f6c14
Thomas Casteleyn <thomas.casteleyn@me.com>
2011-02-28 23:00:09 +0100

    Process obstacles on the map

--- frontend/scripts/engine.js
+++ frontend/scripts/engine.js
@@ -1,10 +1,12 @@
 var target,myChar;
 var myID;
-var parameters = {};
+var scale;
+var obstacles = {};
 var sprites = new Array();
 var debug = (window.console && true);
 
 myID = 's1';
+scale = 32;
 
 function $(id){
 	return document.getElementById(id);
@@ -14,6 +16,27 @@ window.onload = function(){
 	myChar = $(myID);
 	target = $('target');
 
+	var obj, list = document.getElementsByClassName('keepout');
+	var width, height, top, left;
+	for(var i=0; i<list.length; i++){
+		obj = list[i];
+
+		width = obj.getBoundingClientRect().width;
+		height = obj.getBoundingClientRect().height;
+
+		top = left = 0;
+		do{
+			top += obj.offsetTop;
+			left += obj.offsetLeft;
+		}while(obj = obj.offsetParent);
+
+		if(debug) console.log(top,left);
+
+		for(var x=0; x<width; x+=scale) for(var y=0; y<height; y+=scale){
+			obstacles[ (left+x) +','+ (top+y) ] = Infinity;
+		}
+	}
+
 	$('world').onclick = function(mouse){
 
 		if(mouse.target.className == 'keepout') return;
@@ -21,13 +44,13 @@ window.onload = function(){
 		var x = mouse.clientX + window.scrollX - 15.5;
 		var y = mouse.clientY + window.scrollY - 15.5;
 
-		x = Math.round(x/16)*16;
-		y = Math.round(y/16)*16;
+		x = Math.round(x/scale)*scale;
+		y = Math.round(y/scale)*scale;
 
 		target.style.left	= (x+11) + 'px';
 		target.style.top	= (y+11) + 'px';
 
-		// find path..
+		// process path..
 
 		myChar.style.left	= x + 'px';
 		myChar.style.top	= y + 'px';
