d687be1dfa24748d74e2efe3311d5611b8474690
Thomas Casteleyn <thomas.casteleyn@me.com>
2011-03-09 20:42:24 +0100

    Added a Vector object and first implementation of path finding

--- frontend/scripts/engine.js
+++ frontend/scripts/engine.js
@@ -1,12 +1,11 @@
 var target,myChar;
 var myID;
 var scale;
-var obstacles = {};
-var sprites = new Array();
+var sprites = {}, obstacles = {};
 var debug = (window.console && true);
 
 myID = 's1';
-scale = 32;
+scale = $V(32,32);
 
 function $(id){
 	return document.getElementById(id);
@@ -17,43 +16,70 @@ window.onload = function(){
 	target = $('target');
 
 	var obj, list = document.getElementsByClassName('keepout');
-	var width, height, top, left;
+	var width, height, v;
 	for(var i=0; i<list.length; i++){
 		obj = list[i];
 
 		width = obj.getBoundingClientRect().width;
 		height = obj.getBoundingClientRect().height;
 
-		top = left = 0;
+		v = Vector2D.Zero;
 		do{
-			top += obj.offsetTop;
-			left += obj.offsetLeft;
+			v = v.addXY(obj.offsetLeft, obj.offsetTop);
 		}while(obj = obj.offsetParent);
 
-		if(debug) console.log(top,left);
+		if(debug) console.info('Keepout area:',v,width,height);
 
-		for(var x=0; x<width; x+=scale) for(var y=0; y<height; y+=scale){
-			obstacles[ (left+x) +','+ (top+y) ] = Infinity;
+		for(var x=0; x<width; x+=scale.X) for(var y=0; y<height; y+=scale.Y){
+			obstacles[ v.addXY(x,y).toString() ] = Infinity;
 		}
 	}
+
+	myChar.pos = $V( myChar.offsetLeft, myChar.offsetTop );
 
 	$('world').onclick = function(mouse){
 
 		if(mouse.target.className == 'keepout') return;
+		
+		var destination = $V(
+			mouse.clientX + window.scrollX - scale.X/2,
+			mouse.clientY + window.scrollY - scale.Y/2
+		).snapTo(scale);
 
-		var x = mouse.clientX + window.scrollX - 15.5;
-		var y = mouse.clientY + window.scrollY - 15.5;
+		var directPath = myChar.pos.subtract(destination);
 
-		x = Math.round(x/scale)*scale;
-		y = Math.round(y/scale)*scale;
+		if(debug) console.log( 'Current: ' + myChar.pos );
+		if(debug) console.log( 'Destination: ' + destination );
+		if(debug) console.log( 'Path: ' + directPath.inspect() );
 
-		target.style.left	= (x+11) + 'px';
-		target.style.top	= (y+11) + 'px';
+		startPathFinding(destination);
 
-		// process path..
+		target.style.left	= (destination.X+11) + 'px';
+		target.style.top	= (destination.Y+11) + 'px';
 
-		myChar.style.left	= x + 'px';
-		myChar.style.top	= y + 'px';
+		myChar.style.left	= destination.X + 'px';
+		myChar.style.top	= destination.Y + 'px';
+		myChar.pos = destination;
 	}
+}
 
+/**
+ *
+ */
+function startPathFinding(destination){
+	var directPath = myChar.pos.subtract(destination);
+	var nextPos = myChar.pos;
+	do{
+		directPath = directPath.setModulus(directPath.Modulus-(scale.Modulus/2)).snapTo(scale);
+		nextPos = destination.add(directPath);
+		if(debug) console.log('NextPos: '+nextPos);
+		if(debug){
+			var breadcrumb = document.createElement('div');
+			breadcrumb.style.left = nextPos.X + 'px';
+			breadcrumb.style.top  = nextPos.Y + 'px';
+			breadcrumb.className = 'nextPos';
+			if(obstacles[nextPos.toString()]) breadcrumb.style['background-color'] = 'red';
+			$('world').appendChild(breadcrumb);
+		}
+	}while(!nextPos.equals(destination));
 }
---
+++ frontend/scripts/vector.js
@@ -0,0 +1,247 @@
+
+function Vector2D() {}
+
+Vector2D.prototype = {
+	precision: 1e-6,
+	X:0,Y:0,Modulus:0,Angle:0,
+
+	// Returns element i of the vector
+	e: function(i) {
+		switch(i){
+			case 1: return this.X; break;
+			case 2: return this.Y; break;
+			case 3: return this.Modulus; break;
+			case 4: return this.Angle; break;
+			default: return null;
+		}
+	},
+
+	// Sets the modulus ('length') of the vector
+	setModulus: function(m) {
+		this.Modulus = m;
+		return this.calcXY();
+	},
+
+	// Sets the angle of the vector
+	setAngle: function(a) {
+		this.Angle = a;
+		return this.calcXY();
+	},
+
+	// Sets the modulus and the angle of the vector
+	setMA: function(m,a) {
+		this.Modulus = m;
+		this.Angle = a;
+		return this.calcXY();
+	},
+
+	// Gets the modulus and the angle of the vector
+	getMA: function() {
+		return [this.Modulus,this.Angle];
+	},
+
+	// Sets the X value of the vector
+	setX: function(x){
+		this.X = x;
+		return this.calcMA();
+	},
+
+	// Sets the Y value of the vector
+	setY: function(y){
+		this.Y = y;
+		return this.calcMA();
+	},
+
+	// Sets the X and the Y value of the vector
+	setXY: function(x,y){
+		this.X = x;
+		this.Y = y;
+		return this.calcMA();
+	},
+
+	// Gets the X and the Y value of the vector
+	getXY: function(){
+		return [this.X,this.Y];
+	},
+
+	// Returns true if the vector is equal to the argument
+	equals: function(v) {
+		return Math.abs(this.X - v.X) < this.precision && Math.abs(this.Y - v.Y) < this.precision;
+	},
+
+	// Returns a copy of the vector
+	copy: function() {
+		return Vector2D.createXY(this.X,this.Y);
+	},
+
+	// Calls the iterator for each element of the vector in turn
+	each: function(fn) {
+		fn(this.X, 1);
+		fn(this.Y, 2);
+		fn(this.Modulus, 3);
+		fn(this.Angle, 4);
+	},
+
+	// Returns a new vector created by normalizing the receiver
+	toUnitVector: function() {
+		return Vector2D.createMA(1,this.Angle);
+	},
+
+	// Returns the result of adding the argument to the vector
+	add: function(v) {
+		return Vector2D.createXY(this.X+v.X,this.Y+v.Y);
+	},
+	addXY: function(x,y) {
+		return Vector2D.createXY(this.X+x,this.Y+y);
+	},
+
+	// Returns the result of subtracting the argument from the vector
+	subtract: function(v) {
+		return Vector2D.createXY(this.X-v.X,this.Y-v.Y);
+	},
+
+	// Returns the result of multiplying the elements of the vector by the argument
+	multiply: function(k) {
+		return Vector2D.createXY(this.X*k,this.Y*k);
+	},
+
+	x: function(k) { return this.multiply(k); },
+
+	// Returns the scalar product of the vector with the argument
+	dot: function(v) {
+		return (this.X * v.X) + (this.Y * v.Y);
+	},
+
+	// Returns the vector product of the vector with the argument
+	cross: function(v) {
+		return Vector2D.createXY(this.X * v.X,this.Y * v.Y);
+	},
+
+	// Returns the (absolute) largest element of the vector
+	max: function() {
+		return (Math.abs(this.X) > Math.abs(this.Y))? Math.abs(this.X) : Math.abs(this.Y);
+	},
+
+	// Returns the (absolute) smallest element of the vector
+	min: function() {
+		return (Math.abs(this.X) < Math.abs(this.Y))? Math.abs(this.X) : Math.abs(this.Y);
+	},
+
+	// Returns the index of the first match found
+	indexOf: function(x) {
+		var i = 0;
+		do{
+			if(this.e(++i) == x) return i;
+		}while(i<4);
+		return null;
+	},
+
+	// Returns the result of rounding the elements of the vector
+	roundXY: function() {
+		return Vector2D.createXY(Math.round(this.X),Math.round(this.Y));
+	},
+
+	// Returns the result of rounding the modulus of the vector
+	roundModulus: function() {
+		return Vector2D.createMA(Math.round(this.Modulus),this.Angle);
+	},
+
+	// Returns a copy of the vector with elements set to the given value if they
+	// differ from it by less than precision
+	snapTo: function(snapTo) {
+		var snapX = this.X % snapTo.X;
+		var newX = this.X - snapX;
+		var snapY = this.Y % snapTo.Y;
+		var newY = this.Y - snapY;
+
+
+		return Vector2D.createXY(
+			(Math.abs(snapX) - (snapTo.X/2) > this.precision)? newX+snapTo.X : newX ,
+			(Math.abs(snapY) - (snapTo.Y/2) > this.precision)? newY+snapTo.Y : newY
+		);
+	},
+
+	// Returns a copy of the vector with elements set to the given value if they
+	// differ from it by less than precision
+	snapToXY: function(x,y) {
+		var snapX = this.X % x;
+		var newX = this.X - snapX;
+		var snapY = this.Y % y;
+		var newY = this.Y - snapY;
+
+
+		return Vector2D.createXY(
+			(Math.abs(snapX) - (x/2) > this.precision)? newX+x : newX ,
+			(Math.abs(snapY) - (y/2) > this.precision)? newY+y : newY
+		);
+	},
+
+	// Returns the vector's distance from the argument, when considered as a point in space
+	distanceFrom: function(v) {
+		return this.subtract(v).Modulus;
+	},
+
+	// Rotates the vector with given angle
+	rotate: function(angle) {
+		return Vector2D.createMA(this.Modulus,this.Angle+angle);
+	},
+
+	// Utility to make sure vectors are 3D, a zero z-component is added
+/*	to3D: function() {
+		return Vector3D.create(this.X,this.Y,0);
+	}, /**/
+
+	// Returns a string representation of the vector
+	inspect: function() {
+		return '[Vector2D] X:'+this.X + ' Y:'+this.Y + ' Modulus:'+this.Modulus + ' Angle:'+((this.Angle/Math.PI)*180);
+	},
+
+	toString: function() {
+		return '{'+this.X + ','+this.Y+'}';
+	},
+
+	// Recalculates the X and Y value
+	calcXY: function(){
+		this.X = Math.cos(this.Angle) * this.Modulus;
+		this.Y = Math.sin(this.Angle) * this.Modulus;
+		return this;
+	},
+
+	// Recalculates the Modulus and angle
+	calcMA: function(){
+		this.Modulus = Math.sqrt(Math.pow(this.X,2)+Math.pow(this.Y,2));
+		this.Angle = Math.atan2(this.Y,this.X);
+		return this;
+	}
+};
+
+// Construct with X and Y
+Vector2D.createXY = function(x,y) {
+	var V = new Vector2D();
+	return V.setXY(x,y);
+};
+
+// Constructor with Modulus and Angle
+Vector2D.createMA = function(modulus,angle){
+	var V = new Vector2D();
+	return V.setMA(modulus,angle);
+};
+
+// Default constructor function
+Vector2D.create = Vector2D.createXY;
+
+// unit vectors
+Vector2D.Xaxis = Vector2D.createXY(1,0);
+Vector2D.Yaxis = Vector2D.createXY(0,1);
+Vector2D.Zero = Vector2D.createXY(0,0);
+
+// Random vector
+Vector2D.Random = function(v) {
+	if(typeof v == 'undefined') v = Vector2D.createXY(1,1);
+	var x = Math.random() * v.X;
+	var y = Math.random() * v.Y;
+	return Vector2D.createXY(x,y);
+};
+
+// Utility functions
+var $V = Vector2D.create;
