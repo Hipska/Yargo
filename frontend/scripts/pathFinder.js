893aabcd45f7584cccc4d53e1bc2e115c51f1d76
Thomas Casteleyn <thomas.casteleyn@me.com>
2011-04-08 12:47:11 +0200

    Added a pathfinding mechanism

---
+++ frontend/scripts/pathFinder.js
@@ -0,0 +1,109 @@
+
+function PathFinder(obstacles, scale){
+	this.openList = new Array();
+	this.openListSize = 25;
+
+	this.closedList = obstacles || new VectorList();
+
+	this.scale = scale || Vector2D.Ones;
+
+	this.iterations = 0;
+}
+
+PathFinder.prototype = {
+	calculate: function(start,goal){
+
+		this.start = new Path(start);
+		this.goal = goal;
+
+		this.openList = new Array();
+		this.addToOpenList(this.start);
+
+		this.iterations = 0;
+
+		while(this.openList.length > 0){
+
+			++this.iterations;
+
+			var path = this.openList.shift().path;
+
+			if(path.position.equals(this.goal)){
+				var route = new Array();
+
+				do {
+					if(debug) createBreadcrumb(path.position,'green');
+					route.push(path.position);
+				} while(path = path.parent);
+
+				route.reverse();
+
+				return route;
+
+			}else
+				this.expand(path);
+		}
+
+		return null;
+	},
+
+	expand: function(path){
+
+		// calculate the neighbour positions of current position
+		var neighbours = [
+			path.addStep($V(path.position.X - this.scale.X, path.position.Y - this.scale.Y)),	// up-left
+			path.addStep($V(path.position.X - this.scale.X, path.position.Y + this.scale.Y)),	// down-left
+			path.addStep($V(path.position.X + this.scale.X, path.position.Y + this.scale.Y)),	// down-right
+			path.addStep($V(path.position.X + this.scale.X, path.position.Y - this.scale.Y)),	// up-right
+
+			path.addStep($V(path.position.X, path.position.Y - this.scale.Y)),	// up
+			path.addStep($V(path.position.X + this.scale.X, path.position.Y)),	// right
+			path.addStep($V(path.position.X, path.position.Y + this.scale.Y)),	// down
+			path.addStep($V(path.position.X - this.scale.X, path.position.Y))	// left
+		];
+
+		// add new possible positions to openList
+		for(var i = 0; i < 8; ++i)
+			if(neighbours[i].position.X >= 0 && neighbours[i].position.Y >= 0 ) // && !this.closedList.contains(neighbours[i].position))
+				this.addToOpenList(neighbours[i]);
+	},
+
+	addToOpenList: function(path){
+
+		var i = 0;
+		var state = {};
+
+		if(debug) createBreadcrumb(path.position);
+
+		if(this.closedList.contains(path.position))
+			return;
+
+		state.value = path.cost + this.goal.distanceFrom(path.position);
+		state.path = path;
+
+		while(i < this.openList.length) {
+
+			if(this.openList[i].value > state.value)
+				break;
+
+			++i;
+		}
+
+		if(i < this.openListSize)
+			this.openList.splice(i, -1, state);
+	}
+}
+
+function Path(position, cost, parent){
+	
+	this.position = position || null;
+
+	this.cost = cost || 0;
+
+	this.parent = parent || null;
+}
+
+Path.prototype = {
+	addStep: function(position){
+		return new Path(position, this.cost + this.position.distanceFrom(position), this);
+	}
+}
